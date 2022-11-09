f8126113b776f6e0eb9d48d4a4fc7a244e42b782
Lkhooton <lkhooton@gmail.com>
2022-11-08 19:49:13 -0500

    fixed css and js

--- scripts/main.js
+++ scripts/main.js
@@ -8,8 +8,6 @@ let msg = document.getElementById("msg");
 let tasks = document.getElementById("tasks");
 let add = document.getElementById("add");
 
-
-
 //Form validation
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -23,6 +21,54 @@ let formValidation = () => {
   } else {
     console.log("success");
     msg.innerHTML = "";
+    acceptData();
+    add.setAttribute("data-bs-dismiss", "modal");
+    add.click();
+    (()=> {
+      add.setAttribute("data-bs-dismiss", "");
+    })
   }
 };
 
+//Data and local storage usage
+let data = [];
+
+let acceptData = () => {
+  data.push({
+    text: textInput.value,
+    date: dateInput.value,
+    description: textarea.value,
+  });
+
+  localStorage.setItem("data", JSON.stringify(data));
+  console.log(data);
+  createTasks();
+};
+
+//Make new to do
+let createTasks = () => {
+  tasks.innerHTML = "";
+  data.map((x, y) => {
+    return (tasks.innerHTML += `
+    <div id=${y}>
+          <span class="fw-bold">${x.text}</span>
+          <span class="small text-secondary">${x.date}</span>
+          <p>${x.description}</p>
+  
+          <span class="options">
+            <i onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
+            <i onClick ="deleteTask(this);createTasks()" class="fas fa-trash-alt"></i>
+          </span>
+        </div>
+    `);
+  });
+
+  resetForm();
+};
+
+//Reset form 
+let resetForm = () => {
+  textInput.value = "";
+  dateInput.value = "";
+  textarea.value = "";
+}
